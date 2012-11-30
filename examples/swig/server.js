2b7141fe342fb331d956223965daafdc121064b9
Jannick Garthen <jannick.garthen@gmail.com>
2012-12-01 00:38:55 +0100

    removed swig example changes

--- examples/swig/server.js
+++ examples/swig/server.js
@@ -35,7 +35,7 @@ var config = {
 app.configure('production', function() {
 
 	// Override default flags for minify and concationate of all assets by default
-	config.minassic.flags = false;
+	config.minassic.flags = ['minify', 'concatenate'];
 
 });
 
